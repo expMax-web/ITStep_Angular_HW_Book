a75e214fd1169805590530ca5964b09a6f6c1384
expMax-web <84277100+expMax-web@users.noreply.github.com>
2021-11-13 13:28:08 +0400

    hw start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,10 @@
 import { Component } from '@angular/core';
-import { DataService } from './aboutBook.service';
-
-export interface BookReviewsTypes {
-  title?: string;
-  body?: string;
-}
-[];
-
-export interface BookInfoTypes {
-  book?: string;
-  publishingYear?: string;
-  author?: string;
-  numberOfPages?: string;
-  publishingHouse?: string;
-  about?: string;
-  aboutAuthor?: string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [DataService],
 })
 export class AppComponent {
-  BookReviews: any = {};
-  BookInfo: BookInfoTypes = {};
-  constructor(private data: DataService) {}
-  getBookInfo() {
-    this.data.getData();
-  }
-  getBookReviews() {
-    this.data.getReviews();
-  }
-
-  ngOnInit() {
-    this.BookReviews = this.data.getReviews();
-    this.BookInfo = this.data.getData();
-  }
+  ngOnInit() {}
 }
